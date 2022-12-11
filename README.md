3b8a2efc081cb19b5f9239c295d83f4d9f9875b3
Matt Kulka <matt@lqx.net>
2022-12-11 16:59:59 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pixelfed Docker Images
+
+Forked from the [in-repo Dockerfiles](https://github.com/pixelfed/pixelfed/tree/dev/contrib/docker) primarily because I wanted nginx as the web daemon and something to publish to use in a Helm chart.
+
+## Maintainer
+
+Matt Kulka <matt@lqx.net>
---
+++ start.nginx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PATH=/usr/local/bin:$PATH
+cd /var/www
+
+# Create the storage tree if needed and fix permissions
+cp -r storage.skel/* storage/
+cp -r bootstrap.skel/* bootstrap/
+# chown -R www-data:www-data storage/ bootstrap/
+
+# Need to do this because tmp files
+cp /etc/nginx/nginx.conf /tmp/nginx.conf
+sed -i "s/__PORT__/${APP_PORT}/" /tmp/nginx.conf
+sed -i "s/__SERVERNAME__/${APP_DOMAIN}/" /tmp/nginx.conf
+cp /tmp/nginx.conf /etc/nginx/nginx.conf
+
+# Refresh the environment
+php artisan storage:link
+php artisan horizon:publish
+php artisan route:cache
+php artisan view:cache
+php artisan config:cache
+php artisan migrate --force
+# php artisan import:cities
+php artisan instance:actor
+
+# Finally run everything
+php artisan websockets:serve --host=127.0.0.1 &
+php-fpm -D &
+nginx -g 'daemon off;'
